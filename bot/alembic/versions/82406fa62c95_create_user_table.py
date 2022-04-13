9b4dccf369c8b427939339f1ff62731d0cc1622d
vladbakhteev <bakhteev_v@mail.ru>
2022-04-13 15:08:38 +0500

    Add alembic

---
+++ bot/alembic/versions/82406fa62c95_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 82406fa62c95
+Revises: 
+Create Date: 2022-04-13 14:57:48.400101
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '82406fa62c95'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
