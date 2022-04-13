97df1b85f1c59613cc703fa3482bfafac573e0ec
vladbakhteev <bakhteev_v@mail.ru>
2022-04-13 14:59:18 +0500

    Initial commit

---
+++ bot/src/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import func
+
+from .utils import todict
+
+
+class Base:
+    def __repr__(self):
+        params = ', '.join(f'{k}={v}' for k, v in todict(self).items())
+        return f"{self.__class__.__name__}({params})"
+
+
+Base = declarative_base(cls=Base)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)  # from Telegram
+
+    fullname = Column(String)
+    username = Column(String)
+
+    registration_datetime = Column(DateTime, server_default=func.now())
